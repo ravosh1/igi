2fe13199512f5007aa9330f8dc872b484576964a
Ravi <professional.ravi1@gmail.com>
2019-08-09 18:20:24 +0530

    project created

---
+++ testgithubproject/testgithubproject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testgithubproject.Startup))]
+namespace testgithubproject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
